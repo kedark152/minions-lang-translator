6150cfe7d7bc6afefe1b8c67cf90e8149f88dac4
Kedar Kulkarni <kedar.kulkarni12@gmail.com>
2021-09-23 22:36:08 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// console.log("JavaScript is working!!!");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputDiv = document.querySelector("#output");
+var txtInput = document.querySelector("#txtinput");
+btnTranslate.addEventListener("click",clickHandler);
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text){
+    return serverURL + "?" +"text=" + text;
+}
+
+function errorHandler(error){
+ console.log("error occcured",error);
+ alert("something went wrong in server, please try again sometime...");
+}
+
+function clickHandler(){
+    //    outputDiv.innerText = "Banana Language " + txtInput.value;
+    console.log("button is clicked");
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => 
+        {var translatedText= json.contents.translated; //console.log(json.contents.translated)
+         outputDiv.innerText = translatedText;}   ) //
+    .catch(errorHandler());
+
+}
+
